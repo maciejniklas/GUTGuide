a512c86e54d4c08e132c8e00cdfd8a116f434770
Maciej Niklas <kontakt@mniklas.pl>
2021-02-26 20:02:38 +0100

    Dynamic map updater
    - dynamically update the map in viewport
    - dynamically unload map objects in a certain radius around the camera

--- Assets/Code/Core/MapLoader.cs
+++ Assets/Code/Core/MapLoader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 using Google.Maps;
 using Google.Maps.Coord;
 using GUTGuide.DataStructures;
@@ -15,15 +15,23 @@ namespace GUTGuide.Core
     [RequireComponent(typeof(MapsService))]
     public class MapLoader : MonoBehaviour
     {
+        [Header("General settings")]
+        
         [Tooltip("Global coordinates of Gdańsk University of Technology")]
-        [SerializeField] private LatLng gutCoordinates = new LatLng(54.3714279, 18.6169474);
+        [SerializeField] private LatLng gutCoordinates = new LatLng(54.3714279,18.6169474);
         [Tooltip("Distance in which map objects will be rendered")]
         [SerializeField] [Min(10)] private float renderDistance = 1000f;
+        [Tooltip("Distance after which map objects will be updated")]
+        [SerializeField] [Min(10)] private float updateDistance = 100f;
+        [Tooltip("Angle after which map objects will be updated")]
+        [SerializeField] [Range(1, 90)] private float updateAngle = 10f;
+        [Tooltip("Interval in seconds at which unseen geometry is detected and unloaded")]
+        [SerializeField] [Min(0)] private float unloadUnseenDelay = 5f;
+
+        [Header("References")]
         
-        /// <summary>
-        /// Has the application quit?
-        /// </summary>
-        public bool HasQuit { get; private set; }
+        [SerializeField] private Transform groundPlaneTransform;
+        
         /// <summary>
         /// Was the map initialized?
         /// </summary>
@@ -36,7 +44,11 @@ namespace GUTGuide.Core
         /// <summary>
         /// Event called just before map starts loading
         /// </summary>
-        public UnityEvent OnMapLoadingStarted;
+        public UnityEvent onMapLoadingStarted;
+        /// <summary>
+        /// Event called just before map starts loading
+        /// </summary>
+        public UnityEvent<Vector3, float> onMapUnloaded;
 
         /// <summary>
         /// Reference to the map service
@@ -50,6 +62,30 @@ namespace GUTGuide.Core
         /// Default map objects styling
         /// </summary>
         private GameObjectOptions _mapDefaultStyle;
+        /// <summary>
+        /// Has the application quit?
+        /// </summary>
+        private bool _hasQuit;
+        /// <summary>
+        /// Used to indicate that we need to load the map using the current viewport
+        /// </summary>
+        private bool _needLoading;
+        /// <summary>
+        /// Used to let the unload co-routine that we have loaded additional geometry and that older data could be unloaded
+        /// </summary>
+        private bool _needUnloading;
+        /// <summary>
+        /// Position of the camera the last time the map was loaded
+        /// </summary>
+        private Vector3 _lastCameraPosition;
+        /// <summary>
+        /// Rotation of the camera the last time the map was loaded
+        /// </summary>
+        private Quaternion _lastCameraRotation;
+        /// <summary>
+        /// Handle to coroutine used to remove unneeded areas of the map
+        /// </summary>
+        private Coroutine _unloadUnseenCoroutine;
 
         private void Awake()
         {
@@ -76,14 +112,59 @@ namespace GUTGuide.Core
             LoadMap();
         }
 
+        private void Update()
+        {
+            // Prepare necessary variables
+            var mainCameraTransform = _mainCamera.transform;
+            var mainCameraPosition = mainCameraTransform.position;
+            var cameraDisplacementSqr = (mainCameraPosition - _lastCameraPosition).sqrMagnitude;
+            var cameraMovedAngle = Quaternion.Angle(mainCameraTransform.rotation, _lastCameraRotation);
+
+            // Check if loading is needed
+            if (cameraDisplacementSqr > Mathf.Pow(updateDistance, 2) || cameraMovedAngle > updateAngle)
+            {
+                _needLoading = true;
+            }
+
+            if (!_needLoading) return;
+            
+            // Update ground plane position
+            groundPlaneTransform.position = new Vector3(mainCameraPosition.x,
+                groundPlaneTransform.position.y, mainCameraPosition.z);
+                
+            LoadMap();
+
+            // Update fields connected with map loading
+            _lastCameraPosition = mainCameraPosition;
+            _lastCameraRotation = mainCameraTransform.rotation;
+            _needLoading = false;
+            _needUnloading = true;
+        }
+
         private void OnApplicationQuit()
         {
-            HasQuit = true;
+            _hasQuit = true;
         }
 
         private void OnDestroy()
         {
-            OnMapLoadingStarted.RemoveAllListeners();
+            onMapLoadingStarted.RemoveAllListeners();
+            onMapUnloaded.RemoveAllListeners();
+        }
+
+        private void OnDisable()
+        {
+            // Stop detecting if something is to unload
+            if (_unloadUnseenCoroutine == null) return;
+            
+            StopCoroutine(_unloadUnseenCoroutine);
+            _unloadUnseenCoroutine = null;
+        }
+
+        private void OnEnable()
+        {
+            // Start detecting if something is to unload
+            _unloadUnseenCoroutine = StartCoroutine(UnloadUnseenCoroutine());
         }
 
         /// <summary>
@@ -92,14 +173,37 @@ namespace GUTGuide.Core
         private void LoadMap()
         {
             // If app is closed do nothing
-            if (HasQuit) return;
+            if (_hasQuit) return;
 
             // Initial steps
             IsLoading = true;
-            OnMapLoadingStarted?.Invoke();
+            onMapLoadingStarted?.Invoke();
 
             // Load objects in viewport
             _mapsService.MakeMapLoadRegion().AddViewport(_mainCamera, renderDistance).Load(_mapDefaultStyle);
+        }
+
+        /// <summary>
+        /// Periodically remove unneeded areas of the map
+        /// </summary>
+        private IEnumerator UnloadUnseenCoroutine()
+        {
+            while (true)
+            {
+                if (IsInitialized && _needUnloading)
+                {
+                    var mainCameraPosition = _mainCamera.transform.position;
+                    
+                    // Unload map regions that are not in viewport and are outside a radius around the camera
+                    _mapsService.MakeMapLoadRegion().AddCircle(mainCameraPosition, renderDistance)
+                        .UnloadOutside();
+                    onMapUnloaded?.Invoke(mainCameraPosition, renderDistance);
+
+                    _needUnloading = false;
+                }
+
+                yield return new WaitForSeconds(unloadUnseenDelay);
+            }
         }
     }
 }
