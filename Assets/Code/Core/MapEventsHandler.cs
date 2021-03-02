2d262d2573d34547067ec8cf6b2521986439e4e3
Maciej Niklas <kontakt@mniklas.pl>
2021-03-02 19:40:12 +0100

    Terrain elevation
    - back to the default render pipeline
    - using Unity terrain for map generation

---
+++ Assets/Code/Core/MapEventsHandler.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using Google.Maps;
+using Google.Maps.Event;
+using GUTGuide.DataStructures;
+using GUTGuide.Utilities;
+using UnityEngine;
+
+namespace GUTGuide.Core
+{
+    /// <summary>
+    /// The central component for map generated events handling
+    /// </summary>
+    public class MapEventsHandler : MonoBehaviour
+    {
+        /// <summary>
+        /// Default map objects styling
+        /// </summary>
+        private GameObjectOptions _mapDefaultStyle;
+
+        private void Start()
+        {
+            // Obtain default map objects styling
+            _mapDefaultStyle = MapStyleProvider.Instance.GetMapStyle(MapStyleData.Type.Default);
+        }
+
+        /// <summary>
+        /// Handle <see cref="TerrainEvents.AlphaMapsNeedPaint"/> event by specifying setting the painting coroutine.
+        /// </summary>
+        public void HandleAlphaMapsNeedPaint(AlphaMapsNeedPaintArgs arguments)
+        {
+            arguments.PaintingCoroutine = PaintControlTexture(arguments);
+        }
+
+        /// <summary>
+        /// Handle <see cref="AreaWaterEvents.WillCreate"/> event by specifying the area water styles.
+        /// </summary>
+        public void HandleWillCreateAreaWater(WillCreateAreaWaterArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.AreaWaterStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="ExtrudedStructureEvents.WillCreate"/> event by specifying the extruded structure styles.
+        /// </summary>
+        public void HandleWillCreateExtrudedStructure(WillCreateExtrudedStructureArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.ExtrudedStructureStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="IntersectionEvents.WillCreate"/> event by specifying the intersection styles.
+        /// </summary>
+        public void HandleWillCreateIntersection(WillCreateIntersectionArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.SegmentStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="LineWaterEvents.WillCreate"/> event by specifying the line water styles.
+        /// </summary>
+        public void HandleWillCreateLineWater(WillCreateLineWaterArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.LineWaterStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="ModeledStructureEvents.WillCreate"/> event by specifying the modeled structure styles.
+        /// </summary>
+        public void HandleWillCreateModeledStructure(WillCreateModeledStructureArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.ModeledStructureStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="RegionEvents.WillCreate"/> event by specifying the region styles.
+        /// </summary>
+        public void HandleWillCreateRegion(WillCreateRegionArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.RegionStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="SegmentEvents.WillCreate"/> event by specifying the segment styles.
+        /// </summary>
+        public void HandleWillCreateSegment(WillCreateSegmentArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.SegmentStyle;
+        }
+
+        /// <summary>
+        /// Handle <see cref="SegmentEvents.WillCreate"/> event by specifying the segment styles.
+        /// </summary>
+        public void HandleWillCreateTerrain(WillCreateTerrainArgs arguments)
+        {
+            arguments.Style = _mapDefaultStyle.TerrainStyle;
+        }
+
+        /// <summary>
+        /// Coroutine to paint control texture
+        /// </summary>
+        /// <param name="arguments">Arguments from painting event</param>
+        private IEnumerator PaintControlTexture(AlphaMapsNeedPaintArgs arguments)
+        {
+            // Get a temporary render texture to be painted onto
+            var destinationTexture = arguments.Terrain.terrainData.GetAlphamapTexture(0);
+            var renderTextureDescriptor = new RenderTextureDescriptor(destinationTexture.width,
+                destinationTexture.height, RenderTextureFormat.ARGB32, 0);
+            var temporaryRenderTexture = RenderTexture.GetTemporary(renderTextureDescriptor);
+            arguments.RegisterFinalizer(() => RenderTexture.ReleaseTemporary(temporaryRenderTexture));
+
+            yield return null;
+            
+            // Make the active render texture our temporary one, storing the current one for restoration once we're done
+            var renderTexture = RenderTexture.active;
+            RenderTexture.active = temporaryRenderTexture;
+
+            // Paint the control texture from the feature mask using the configured material/shader
+            Graphics.Blit(arguments.FeatureMaskRenderTexture, temporaryRenderTexture,
+                MapStyleProvider.Instance.TerrainControlTexture);
+
+            destinationTexture.ReadPixels(new Rect(0, 0, destinationTexture.width, destinationTexture.height), 0, 0);
+            destinationTexture.Apply();
+            
+            arguments.Terrain.terrainData.SetBaseMapDirty();
+
+            // Restore the previously active render texture
+            RenderTexture.active = renderTexture;
+        }
+    }
+}
--- Assets/Code/Utilities/MapStyleProvider.cs
+++ Assets/Code/Utilities/MapStyleProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Google.Maps;
+using Google.Maps.Feature.Style;
+using Google.Maps.Terrain;
 using GUTGuide.DataStructures;
 using GUTGuide.Patterns;
 using UnityEngine;
@@ -12,8 +15,30 @@ namespace GUTGuide.Utilities
     /// </summary>
     public class MapStyleProvider : PersistentSingleton<MapStyleProvider>
     {
+        [Header("General")]
+        
         [Tooltip("Set of available map styles")]
         [SerializeField] private MapStyleData[] mapStyleData;
+
+        [Header("Terrain settings")]
+        
+        [Tooltip("Material used to paint the control texture from the feature mask")]
+        [SerializeField] private Material terrainControlTexture;
+        [Tooltip("The target resolution of the alpha map for each generated Terrain tile in meters per pixel. " +
+                 "This is the resolution at which TerrainLayer mask painting will occur")]
+        [SerializeField] [Min(0.001f)] private float alphaMapResolutionMetersPerPixel = 1f;
+        [Tooltip("The target resolution in meters per pixel of the composite texture used on the terrain when " +
+                 "viewed from a distance greater than the basemap distance.")]
+        [SerializeField] [Min(0.001f)] private float baseMapResolutionMetersPerPixel = 10f;
+        [Tooltip("The maximum distance at which terrain textures will be displayed at full resolution")]
+        [SerializeField] [Min(1f)] private float baseMapDistance = 2000f;
+        [Tooltip("Terrain layers used to style generated terrain. The first layer is applied by default as the base styling layer")]
+        [SerializeField] private TerrainLayer[] terrainLayers;
+
+        /// <summary>
+        /// Material used to paint the control texture from the feature mask
+        /// </summary>
+        public Material TerrainControlTexture => terrainControlTexture;
 
         /// <summary>
         /// Receive map style
@@ -32,8 +57,46 @@ namespace GUTGuide.Utilities
             }
             
             var style = styleData.GenerateStyle();
-            return style;
 
+            // Terrain style generation
+            var terrainStyleBuilder = new TerrainStyle.Builder()
+            {
+                AlphaMapResolutionMetersPerPixel = alphaMapResolutionMetersPerPixel,
+                BaseMapResolutionMetersPerPixel = baseMapResolutionMetersPerPixel,
+                BaseMapDistance = baseMapDistance,
+                TerrainLayers = new List<TerrainLayer>(terrainLayers)
+            };
+            style.TerrainStyle = terrainStyleBuilder.Build();
+
+            // Segment style with adjustments to the terrain style
+            var segmentStyleBuilder = new SegmentStyle.Builder(style.SegmentStyle)
+            {
+                GameObjectLayer = style.TerrainStyle.TerrainPaintingLayer
+            };
+            style.SegmentStyle = segmentStyleBuilder.Build();
+
+            // Area water style with adjustments to the terrain style
+            var areaWaterStyleBuilder = new AreaWaterStyle.Builder(style.AreaWaterStyle) 
+            {
+                GameObjectLayer = style.TerrainStyle.TerrainPaintingLayer
+            };
+            style.AreaWaterStyle = areaWaterStyleBuilder.Build();
+            
+            // Region style with adjustments to the terrain style
+            var regionStyleBuilder = new RegionStyle.Builder(style.RegionStyle) 
+            {
+                GameObjectLayer = style.TerrainStyle.TerrainPaintingLayer
+            };
+            style.RegionStyle = regionStyleBuilder.Build();
+            
+            // Line water style with adjustments to the terrain style
+            var lineWaterStyleBuilder = new LineWaterStyle.Builder(style.LineWaterStyle) 
+            {
+                GameObjectLayer = style.TerrainStyle.TerrainPaintingLayer
+            };
+            style.LineWaterStyle = lineWaterStyleBuilder.Build();
+            
+            return style;
         }
     }
 }
