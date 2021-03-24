69c6f8c1d12dd498f690d86b23d09ad6bd252b54
Maciej Niklas <kontakt@mniklas.pl>
2021-03-24 20:35:11 +0100

    GUT building UI card

---
+++ Assets/Code/UI/Components/GutBuildingCard.cs
@@ -0,0 +1,39 @@
+﻿using GUTGuide.DataStructures;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GUTGuide.UI.Components
+{
+    /// <summary>
+    /// Controller of the UI representation of the <see cref="GutBuildingData"/>
+    /// </summary>
+    public class GutBuildingCard : MonoBehaviour
+    {
+        [Tooltip("Reference to the text component of the number label")]
+        [SerializeField] private Text numberLabel;
+        [Tooltip("Reference to the text component of the full name label")]
+        [SerializeField] private Text fullNameLabel;
+        [Tooltip("Reference to the text component of the shortcut label")]
+        [SerializeField] private Text shortcutLabel;
+        [Tooltip("Reference to the text component of the address label")]
+        [SerializeField] private Text addressLabel;
+
+        /// <summary>
+        /// Identifier of the building on the map
+        /// </summary>
+        private string _id;
+
+        /// <summary>
+        /// Initially fill the card with the given data
+        /// </summary>
+        /// <param name="gutBuildingData">Dataset of the GUT building</param>
+        public void Initialize(GutBuildingData gutBuildingData)
+        {
+            _id = gutBuildingData.id;
+            numberLabel.text = gutBuildingData.number.ToString("000");
+            fullNameLabel.text = gutBuildingData.fullName;
+            shortcutLabel.text = gutBuildingData.shortcut;
+            addressLabel.text = gutBuildingData.address;
+        }
+    }
+}
--- Assets/Code/UI/Windows/SearchBuildingPanel.cs
+++ Assets/Code/UI/Windows/SearchBuildingPanel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUTGuide.DataStructures;
+using GUTGuide.UI.Components;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -17,9 +18,18 @@ namespace GUTGuide.UI.Windows
         [Tooltip("The prefab of the GUT building list card")]
         [SerializeField] private GameObject gutBuildingCardPrefab;
 
+        /// <summary>
+        /// Reference to the <see cref="Animator"/> of this object
+        /// </summary>
         private Animator _animator;
         
+        /// <summary>
+        /// Hash code of the hide animation
+        /// </summary>
         private static readonly int HideAnimationTrigger = Animator.StringToHash("Hide");
+        /// <summary> 
+        /// Hash code of the display animation
+        /// </summary>
         private static readonly int ShowAnimationTrigger = Animator.StringToHash("Show");
 
         private void Awake()
@@ -27,11 +37,29 @@ namespace GUTGuide.UI.Windows
             _animator = GetComponent<Animator>();
         }
 
+        private void Start()
+        {
+            // Fill panel with the GUT buildings data in the form of info cards
+            foreach (var gutBuildingData in Resources.LoadAll<GutBuildingData>("GUTBuildingsData"))
+            {
+                var gutBuildingCardObject = Instantiate(gutBuildingCardPrefab, searchBuildingPanelContentTransform);
+                var gutBuildingCard = gutBuildingCardObject.GetComponent<GutBuildingCard>();
+
+                gutBuildingCard.Initialize(gutBuildingData);
+            }
+        }
+
+        /// <summary>
+        /// Hide panel from the app viewport
+        /// </summary>
         public void Hide()
         {
             _animator.SetTrigger(HideAnimationTrigger);
         }
 
+        /// <summary>
+        /// Display panel to the app viewport
+        /// </summary>
         public void Show()
         {
             _animator.SetTrigger(ShowAnimationTrigger);
