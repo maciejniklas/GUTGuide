4eed20d7da2bc21e7dc08c4d953dd0ca7eb14182
Maciej Niklas <kontakt@mniklas.pl>
2021-03-30 21:50:03 +0200

    Compass

---
+++ Assets/Code/UI/Components/CompassNeedle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace GUTGuide.UI.Components
+{
+    public class CompassNeedle : MonoBehaviour
+    {
+        private Transform _mainCameraTransform;
+
+        private void Awake()
+        {
+            if (!(Camera.main is null)) _mainCameraTransform = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            transform.localRotation = Quaternion.Euler(Vector3.forward * _mainCameraTransform.rotation.eulerAngles.y);
+        }
+    }
+}
